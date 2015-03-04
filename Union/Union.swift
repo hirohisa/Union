e1066d126155d7e046edcae3549829aefc6eda35
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-03-05 03:07:22 +0900

    Use `AppleLocale en_US`

--- Union/Union.swift
+++ Union/Union.swift
@@ -2,8 +2,8 @@
 //  Union.swift
 //  Union
 //
-//  Created by Hirohisa Kawasaki on 2015/02/19.
-//  Copyright (c) 2015年 Hirohisa Kawasaki. All rights reserved.
+//  Created by Hirohisa Kawasaki on 2/19/15.
+//  Copyright (c) 2015 Hirohisa Kawasaki. All rights reserved.
 //
 
 import UIKit
